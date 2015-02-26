b32f59c351f4ec541217447524a05a9219322a95
sguha-work <sguha1988.life@gmail.com>
2015-02-26 14:57:35 +0530

    comments added for the request of home page of main site

--- Frontend/init.js
+++ Frontend/init.js
@@ -1,4 +1,15 @@
 // this files handle all the requests for frontend
+
+/**
+*	points to be noted
+*	the final return value mast be a json object like the following
+*	{
+*		isFile : 1,
+*		filePath : 'Frontend/main_site/html/index.html'
+*	}
+*	based on the object the output will be placed on browser
+*
+*/
 module.exports = {
 	processRequest : function(requestURL) {
 		if(requestURL == '/') {
@@ -12,7 +23,7 @@ module.exports = {
 	},
 	mainSiteHomePageRequest : function() {
 		var outputObject = {
-			isFile : 1,
+			isFile : 1, // determining that the return value holds a file path and the response will be a file
 			filePath : 'Frontend/main_site/html/index.html'
 		};
 		return outputObject;
