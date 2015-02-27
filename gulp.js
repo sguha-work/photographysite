fc3a0070b16675ec9faddb9eb005f4583d8a4d95
sguha-work <sguha1988.life@gmail.com>
2015-02-27 18:37:49 +0530

    gulp test

---
+++ gulp.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'); 
+// include plug-ins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+ 
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+  gulp.src(['./Frontend/MainSite/js/dev/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./Frontend/MainSite/js/'));
+});
+
+// default gulp task
+gulp.task('default', ['scripts'], function() {
+});
+
+// default gulp task
+// gulp.task('default', ['scripts'], function() {
+   
+//   // watch for JS changes
+//   gulp.watch('./Frontend/MainSite/js/dev/**/*.js', function() {
+//     gulp.run('scripts');
+//   });
+ 
+  
+// });
