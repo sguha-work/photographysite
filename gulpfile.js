b756578120537934223dcbfa1758c3a8e3442822
sguha-work <sguha1988.life@gmail.com>
2015-03-02 12:48:33 +0530

    folder support added for gulp html minifier.other problem is still active

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('ADMIN_PANEL_JAVASCRIPT_MINIFIER', function() {
 });
 
 gulp.task('MAIN_SITE_HTML_MINIFIER', function() {
-  var htmlSrc = './Frontend/MainSite/html/dev/*.html',
+  var htmlSrc = './Frontend/MainSite/html/dev/**/*.html',
       htmlDst = './Frontend/MainSite/html/prod';
  
   gulp.src(htmlSrc)
@@ -28,33 +28,23 @@ gulp.task('MAIN_SITE_HTML_MINIFIER', function() {
 });
 
 gulp.task('ADMIN_PANEL_HTML_MINIFIER', function() {
-  var htmlSrc = './Frontend/AdminPanel/html/dev/*.html',
+  var htmlSrc = './Frontend/AdminPanel/html/dev/**/*.html',
       htmlDst = './Frontend/AdminPanel/html/prod';
  
   gulp.src(htmlSrc)
     .pipe(minifyHTML())
     .pipe(gulp.dest(htmlDst));
 });
-// default gulp task
-gulp.task('default', ['MAIN_SITE_JAVASCRIPT_MINIFIER', 'MAIN_SITE_HTML_MINIFIER','ADMIN_PANEL_JAVASCRIPT_MINIFIER', 'ADMIN_PANEL_HTML_MINIFIER'], function() {
-});
+
 
 // default gulp task
 gulp.task('default', ['MAIN_SITE_JAVASCRIPT_MINIFIER', 'MAIN_SITE_HTML_MINIFIER', 'ADMIN_PANEL_JAVASCRIPT_MINIFIER','ADMIN_PANEL_HTML_MINIFIER'], function() {
    
   // watch for JS changes
-  gulp.watch('./Frontend/MainSite/js/dev/**/*.js', function() {
-    gulp.run('MAIN_SITE_JAVASCRIPT_MINIFIER');
-  });
-  gulp.watch('./Frontend/AdminPanel/js/dev/**/*.js', function() {
-    gulp.run('ADMIN_PANEL_JAVASCRIPT_MINIFIER');
-  });
+  gulp.watch('./Frontend/MainSite/js/dev/**/*.js', ['MAIN_SITE_JAVASCRIPT_MINIFIER']);
+  gulp.watch('./Frontend/AdminPanel/js/dev/**/*.js', ['ADMIN_PANEL_JAVASCRIPT_MINIFIER']);
   //watch the html changes
-  gulp.watch('./Frontend/MainSite/html/dev/*.html', function() {
-    gulp.run('MAIN_SITE_HTML_MINIFIER');
-  });
-  gulp.watch('./Frontend/AdminPanel/html/dev/*.html', function() {
-    gulp.run('ADMIN_PANEL_HTML_MINIFIER');
-  });
+  gulp.watch('./Frontend/MainSite/html/dev/**/*.html', ['MAIN_SITE_HTML_MINIFIER']);
+  gulp.watch('./Frontend/AdminPanel/html/dev/**/*.html', ['ADMIN_PANEL_HTML_MINIFIER']);
   
 });
